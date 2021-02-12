10fee80422b6a5859492d67860d627c67ecc5561
root <ric_bap@hotmail.com>
2021-02-12 08:52:33 -0300

    Iniciando

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+
+  public paginas = [
+    { titulo:'Home',  url:'home', icone: 'home' },
+    { titulo:'Instalação', url:'instalacao', icone:'home' }
+  ]
+  
+  constructor() {}
+}
