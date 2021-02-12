815bd29edee63eb42fba11501c3c3519736afd18
root <ric_bap@hotmail.com>
2021-02-12 11:58:33 -0300

    Layout Split Pane

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ export class AppComponent {
 
   public paginas = [
     { titulo:'Home',  url:'home', icone: 'home' },
-    { titulo:'Instalação', url:'instalacao', icone:'home' },
-    { titulo:'Layout', url:'layout', icone:'home' }
+    { titulo:'Instalação', url:'instalacao', icone:'arrow-forward-circle' },
+    { titulo:'Layout', url:'layout', icone:'arrow-forward-circle' }
   ]
   
   constructor() {}
